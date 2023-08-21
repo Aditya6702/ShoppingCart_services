5063988890309ba9089fd3a7942e829c0b737c87
Diablo <92186346+Aditya6702@users.noreply.github.com>
2023-08-21 20:01:48 +0530

    Update ProductRepository.java

--- oops-lab/src/main/java/com/Project/oopslab/repository/ProductRepository.java
+++ oops-lab/src/main/java/com/Project/oopslab/repository/ProductRepository.java
@@ -2,6 +2,6 @@ package com.Project.oopslab.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.Project.oopslab.model.product;
-
+//interface to extend the MongoRepository interface with funnction to run CRUD on manngoDb 
 public interface ProductRepository extends MongoRepository<product,String> {
 }
