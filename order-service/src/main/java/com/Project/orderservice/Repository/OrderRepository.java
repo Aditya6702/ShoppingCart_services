f8801f9fcea92b579a37197276f9374c5645f100
Diablo <92186346+Aditya6702@users.noreply.github.com>
2023-08-21 20:13:38 +0530

    Update OrderRepository.java

--- order-service/src/main/java/com/Project/orderservice/Repository/OrderRepository.java
+++ order-service/src/main/java/com/Project/orderservice/Repository/OrderRepository.java
@@ -2,6 +2,6 @@ package com.Project.orderservice.Repository;
 
 import com.Project.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//this interface extends the sql interface from sprinng boot
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
