bb87564c247db3a67561a63bd6d832a62d0bc48e
Diablo <92186346+Aditya6702@users.noreply.github.com>
2023-08-21 19:58:23 +0530

    Update ProductController.java

--- oops-lab/src/main/java/com/Project/oopslab/Controller/ProductController.java
+++ oops-lab/src/main/java/com/Project/oopslab/Controller/ProductController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/product")
-@RequiredArgsConstructor
+@RestController//used by the sprinng framework to indicate the class is a controller that handles htttp requests
+@RequestMapping("/api/product")//it defines how the http request should be mapped
+@RequiredArgsConstructor//lombok annotation
 public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody Productrequest productrequest) {
+    @PostMapping//used to add and update
+    @ResponseStatus(HttpStatus.CREATED)//gives a http created status
+    public void createProduct(@RequestBody Productrequest productrequest) {//RequestBody indicates methodf parameters should be bound to the http request body
         productService.createproduct(productrequest);
     }
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
+    @GetMapping//used to retrive the data
+    @ResponseStatus(HttpStatus.OK)//Creates ok http status
     public List<ProductResponse> getAllProducts(){
     return productService.getAllProducts();
     }
